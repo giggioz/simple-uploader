56b86559d7c670dc32a6d5b234b2c71c6eeb93f8
Giggioz <giggioz@gmail.com>
2015-11-24 02:09:18 +0100

    update package

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   name: 'giggioz:simple-uploader',
   version: '0.0.2',
   summary: 'A simple way to create fancy styled file inputs with some sugar about security',
-  git: '',
+  git: 'https://github.com/giggioz/simple-uploader',
   documentation: 'README.md'
 });
 
